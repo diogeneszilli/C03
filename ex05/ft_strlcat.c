0e6cad0898791d9a75da5a1299fd547d580f1538
dludtke- <dludtke-@student.42sp.org.br>
2021-04-10 18:22:42 +0000

    fix ex05

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -6,27 +6,37 @@
 /*   By: dludtke- <dludtke-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 19:18:49 by dludtke-          #+#    #+#             */
-/*   Updated: 2021/04/09 22:53:28 by dludtke-         ###   ########.fr       */
+/*   Updated: 2021/04/09 23:24:10 by dludtke-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+unsigned int	ft_strlen(char *str)
 {
-	unsigned int dest_size;
 	unsigned int i;
 
-	dest_size = 0;
 	i = 0;
-	while (dest[dest_size] != '\0')
-		dest_size++;
-	if (size > 0)
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+	unsigned int dest_size;
+	unsigned int src_size;
+
+	dest_size = ft_strlen(dest);
+	src_size = ft_strlen(src);
+	if (size <= dest_size)
+		return (size + src_size);
+	i = 0;
+	while (src[i] != '\0' && dest_size + 1 < size)
 	{
-		while (i < (dest_size + size - 1))
-		{
-			dest[dest_size + i] = src[i];
-			i++;
-		}
-		dest[dest_size + i] = '\0';
+		dest[dest_size] = src[i];
+		i++;
+		dest_size++;
 	}
-	return (dest_size + size);
+	dest[dest_size] = '\0';
+	return (dest_size + ft_strlen(&src[i]));
 }
