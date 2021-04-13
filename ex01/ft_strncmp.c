7cf4182f53e7b919df16dd5f3cf14a9cb4b323be
dludtke- <dludtke-@student.42sp.org.br>
2021-04-13 13:01:52 +0000

    fix ex01

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dludtke- <dludtke-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 18:38:51 by dludtke-          #+#    #+#             */
-/*   Updated: 2021/04/12 21:31:46 by dludtke-         ###   ########.fr       */
+/*   Updated: 2021/04/13 12:56:40 by dludtke-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@ int		ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while (((i < n) && (s1[i] && s2[i]) &&
-					(s1[i] == s2[i])) && (s1[i] == '\0' && s2[i] == '\0'))
+	while ((i < n) && (s1[i] && s2[i]) && (s1[i] == s2[i]))
 		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	if (i == n)
+		return (0);
+	else
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
